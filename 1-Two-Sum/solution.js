cfccd2760b616f328269cd521437ab68bfd77b48
Alejandro Garcia Salas <garciasalasalejandro@gmail.com>
2019-07-24 20:03:24 -0700

    cleanup

--- 1-Two-Sum/solution.js
+++ 1-Two-Sum/solution.js
@@ -1,18 +1,18 @@
 function twoSum(nums, target) {
-	const complementToIndex = {};
-	for (let index = 0; index < nums.length; index++) {
-		const complement = target - nums[index];
-		complementToIndex[complement] = index;
-	}
+  const complementToIndex = {};
+  for (let index = 0; index < nums.length; index++) {
+    const complement = target - nums[index];
+    complementToIndex[complement] = index;
+  }
 
-	for (let i = 0; i < nums.length; i++) {
-		const curr = nums[i];
-		if (complementToIndex[curr]) {
-			if (complementToIndex[curr] !== i) { // it's not the same element, for example [1, 3, 4, 2] with target = 6
-				return ([i, complementToIndex[curr]]);
-			}
-		}
-	}
+  for (let i = 0; i < nums.length; i++) {
+    const curr = nums[i];
+    if (complementToIndex[curr]) {
+      if (complementToIndex[curr] !== i) { // it's not the same element, for example [1, 3, 4, 2] with target = 6
+        return ([i, complementToIndex[curr]]);
+      }
+    }
+  }
 }
 
 // Tests
